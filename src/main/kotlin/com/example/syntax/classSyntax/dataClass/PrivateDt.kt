437aecdfd0057ec912a657d952c477c186da8028
pius712 <aksfbsgnlfls@gmail.com>
2023-09-28 19:04:14 +0900

    data class private 멤버는 get, set 불가.

---
+++ src/main/kotlin/com/example/syntax/classSyntax/dataClass/PrivateDt.kt
@@ -0,0 +1,4 @@
+package com.example.syntax.classSyntax.dataClass
+
+data class PrivateDt(private val name:String, private val age: Int) {
+}
---
+++ src/test/kotlin/com/example/syntax/classSyntax/dataClass/PrivateDtTest.kt
@@ -0,0 +1,11 @@
+package com.example.syntax.classSyntax.dataClass
+
+import org.junit.jupiter.api.Test
+
+class PrivateDtTest {
+
+    @Test
+    fun propertyTest() {
+        val dt = PrivateDt("kim", 10)
+    }
+}
